f2ab9a6904d1255bc583f445e6bce9f03195c7fc
Nikita <emper137137@gmail.com>
2025-06-09 22:02:08 +1000

    sync

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,9 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import 'dotenv/config';
+import 'solidity-coverage';
+
+// https://youtu.be/gyMwXuJrbJQ?t=33937
 
 const config: HardhatUserConfig = {
   solidity: '0.8.28',
